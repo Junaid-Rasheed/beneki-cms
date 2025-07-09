c41e00cd68086225b41f90e6aafd29a6c6fab8a3
Hammad Shaukat <hshaukat52@gmail.com>
2025-07-10 00:21:54 +0500

    email configuration implementation

--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'Admin@beneki.com',
+        defaultReplyTo: 'support@yourdomain.com',
+      },
+    },
+  },
   // ...
 });
