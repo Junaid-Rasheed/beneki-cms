dd367f5efb279195ff2372acad50e1d26ef3ab25
Hammad Shaukat <hshaukat52@gmail.com>
2025-09-11 06:20:03 +0500

    minor fixes

--- src/api/bnpaxepta/controllers/bnpaxepta.js
+++ src/api/bnpaxepta/controllers/bnpaxepta.js
@@ -80,22 +80,22 @@ module.exports = {
 
     // Pad to 12 digits
     const refNr = numberPart.padStart(12, "0");
-    const browserInfo = `{"acceptHeaders":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","ipAddress":"223.123.20.53","javaEnabled":false,"javaScriptEnabled":true,"language":"en-PK","colorDepth":24,"screenHeight":1080,"screenWidth":1920,"timeZoneOffset":"-300","userAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"}`;
     // Build parameter string with proper encoding
-    const clearParams = 
-      `MerchantID=${merchantId}` +
-      `&TransID=${orderId}` +
-      `&MsgVer=2.0` +
-      `&RefNr=${refNr}` +
-      `&Amount=${amountMinor}` +
-      `&Currency=${currency}` +
-      `&browserInfo={"acceptHeaders":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8","ipAddress":"223.123.20.53","javaEnabled":false,"javaScriptEnabled":true,"language":"en-PK","colorDepth":24,"screenHeight":1080,"screenWidth":1920,"timeZoneOffset":"-300","userAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"}` +
-      `&URLSuccess=${process.env.URL_SUCCESS}` +
-      `&URLFailure=${process.env.URL_FAILURE}` +    
-      `&URLNotify=${process.env.URL_NOTIFY}` +
-      `&MAC=${mac}`;
+    const clearParams = [
+      `MerchantID=${merchantId}`,
+      `TransID=${orderId}`,
+      `MsgVer=2.0`,
+      `RefNr=${refNr}`,
+      `Amount=${amountMinor}`,
+      `Currency=${currency}`,
+      `browserInfo=${normalized}`,
+      `URLSuccess=${process.env.URL_SUCCESS}`,
+      `URLFailure=${process.env.URL_FAILURE}`,    
+      `URLNotify=${process.env.URL_NOTIFY}`,
+      `MAC=${mac}`
+    ].join("&");
 
-     strapi.log.info("Data"+ JSON.stringify(clearParams));
+     strapi.log.info("Data"+ clearParams);
     // LEN = byte length of UNENCRYPTED string
     const len = Buffer.byteLength(clearParams, "utf8");
 
